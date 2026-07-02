8a2cefaff8365453cfbbe143304f045d717e7f6b
Fernando Antunes <fernando.antunes@processor.com.br>
2026-07-02 16:55:18 -0300

    feat: configurar EF Core com SQLite e AppDbContext
    - adiciona pacotes Microsoft.EntityFrameworkCore.Sqlite e Microsoft.EntityFrameworkCore.Design
    - cria AppDbContext para mapear Produto
    - registra DbContext no Program.cs com UseSqlite
    - prepara base para migrations e persistência em banco

---
+++ MiniErp.Api/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniErp.Api.Models;
+
+namespace MiniErp.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Produto> Produtos => Set<Produto>();
+}
