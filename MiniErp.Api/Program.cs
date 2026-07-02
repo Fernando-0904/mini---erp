1cc2d509d7c61623f34fee72c22650ef23469b99
Fernando Antunes <fernando.antunes@processor.com.br>
2026-07-02 13:57:34 -0300

    chore: remover dependência vulnerável do OpenApi
    - remove pacote Microsoft.AspNetCore.OpenApi da API
    
    - remove chamadas AddOpenApi e MapOpenApi
    
    - evita aviso de vulnerabilidade do Microsoft.OpenApi
    
    - exclui pasta MiniErp.Api da compilação do projeto console

--- MiniErp.Api/Program.cs
+++ MiniErp.Api/Program.cs
@@ -3,16 +3,10 @@ using MiniErp.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
 builder.Services.AddCors();
 builder.Services.AddSingleton<ProdutoService>();
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
